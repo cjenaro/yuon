a15888d69ab827b9342bbb9f7f476c515044becf
cjenaro <jen.calvineo@gmail.com>
2025-04-09 11:48:33 -0300

    feat: pages controller

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,58 @@
+class PagesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_page, only: [:show, :edit, :update, :destroy]
+  
+  def index
+    @pages = Current.user.pages.where(parent_page_id: nil)
+  end
+  
+  def show
+  end
+  
+  def new
+    @page = Page.new
+    @page.parent_page_id = params[:parent_page_id] if params[:parent_page_id]
+  end
+  
+  def create
+    @page = Current.user.pages.new(page_params)
+    
+    if @page.save
+      redirect_to @page, notice: "Page was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+  
+  def edit
+  end
+  
+  def update
+    if @page.update(page_params)
+      redirect_to @page, notice: "Page was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+  
+  def destroy
+    @page.destroy
+    redirect_to pages_path, notice: "Page was successfully deleted."
+  end
+  
+  private
+  
+  def set_page
+    @page = Current.user.pages.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    redirect_to pages_path, alert: "Page not found or you don't have access to it."
+  end
+  
+  def page_params
+    params.require(:page).permit(:title, :parent_page_id, :is_public)
+  end
+  
+  def authenticate_user!
+    redirect_to new_session_path, alert: "Please sign in to access pages." unless Current.user
+  end
+end 
